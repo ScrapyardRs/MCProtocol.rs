af8c0090067e3b56bc0a09f9744abc7a8cf4240b
Corey Shupe <coreyshupe101@gmail.com>
2022-08-09 02:57:43 -0400

    Progress

--- mc_registry/src/error.rs
+++ mc_registry/src/error.rs
@@ -8,6 +8,8 @@ pub enum Error {
     SerdeError(mc_serializer::serde::Error),
 }
 
+impl std::error::Error for Error {}
+
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match &self {
