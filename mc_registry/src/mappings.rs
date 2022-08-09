2fc2fdb600b2843cda8798b386452cda62f86d82
Corey Shupe <coreyshupe101@gmail.com>
2022-08-09 10:32:58 -0400

    Elide useless lifetimes.

--- mc_registry/src/mappings.rs
+++ mc_registry/src/mappings.rs
@@ -5,8 +5,8 @@ use std::io::Cursor;
 pub trait Mappings {
     type PacketType;
 
-    fn attach_to_register<'a, 'b: 'a, Context>(
-        registry: &'b mut crate::registry::StateRegistry<'a, Context>,
+    fn attach_to_register<'a, Context>(
+        registry: &mut crate::registry::StateRegistry<'a, Context>,
         handle: crate::registry::StateRegistryHandle<'a, Context>,
     );
 
@@ -34,7 +34,7 @@ macro_rules! create_mappings {
         impl $crate::mappings::Mappings for $registrar_type {
             type PacketType = $registrar_type;
 
-            fn attach_to_register<'a, 'b: 'a, Context>(registry: &'b mut $crate::registry::StateRegistry<'a, Context>, handle: $crate::registry::StateRegistryHandle<'a, Context>) {
+            fn attach_to_register<'a, Context>(registry: &mut $crate::registry::StateRegistry<'a, Context>, handle: $crate::registry::StateRegistryHandle<'a, Context>) {
                 registry.attach_mappings::<'a, $registrar_type>(handle);
             }
 
