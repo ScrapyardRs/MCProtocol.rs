e86b77bf26649a8fba7a45fecac8eeb2eddb845e
Corey Shupe <coreyshupe101@gmail.com>
2022-10-10 00:18:10 -0400

    Check play state init.

--- src/protocol/play.rs
+++ src/protocol/play.rs
@@ -1,3 +1,17 @@
 pub mod sb {}
 
-pub mod cb {}
+pub mod cb {
+    use crate::chat::Chat;
+
+    #[derive(drax_derive::DraxTransport)]
+    pub struct Disconnect {
+        #[drax(json = 32767)]
+        pub reason: Chat,
+    }
+
+    crate::import_registrations! {
+        Disconnect {
+            760 -> 0x19,
+        }
+    }
+}
